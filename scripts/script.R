7138e7144e179a4e9ac03c4777be222b4219cabf
marcoscattolin <marco.scattolin@gmail.com>
2018-01-09 13:17:49 +0100

    - fixed quotes when reading - filtered out placeholders longer than 3 digits

--- scripts/script.R
+++ scripts/script.R
@@ -5,21 +5,43 @@ library(readxl)
 
 
 
+# READ FILES --------------------------------------------------------------
 clx <- read_excel("k:/dept/DIGITAL E-COMMERCE/E-COMMERCE/Report E-Commerce/pim_report/raw data/Prada_report.xlsx", sheet = "Prada") %>% 
         bind_rows(read_excel("k:/dept/DIGITAL E-COMMERCE/E-COMMERCE/Report E-Commerce/pim_report/raw data/Prada_report.xlsx", sheet = "Miu Miu"))
 
-anagrafica <- read_csv2("k:/dept/DIGITAL E-COMMERCE/E-COMMERCE/Report E-Commerce/pim_report/raw data/FLUIID4_ANAGRAFICA_TOT.CSV", col_types = cols(.default = col_character()))
+anagrafica <- read_csv2("k:/dept/DIGITAL E-COMMERCE/E-COMMERCE/Report E-Commerce/pim_report/raw data/FLUIID4_ANAGRAFICA_TOT.CSV",quote = "", col_types = cols(.default = col_character()))
 
 
+
+# REMOVE QUOTES -----------------------------------------------------------
+anagrafica <- anagrafica %>% 
+        mutate_all(~ gsub("\"$","",.)) %>% 
+        mutate_all(~ gsub("^\"","",.))
+
+
+
+# FILTER OUT INVALID PLACEHOLDERS  ----------------------------------------
+clx <- clx %>% 
+        separate(col = "file_name",into = c("tmp","placeholder"), extra = "drop", sep = "_") %>% 
+        select(-tmp) %>% 
+        filter(nchar(placeholder) == 3)
+
+
+
+
+# WRITE FILES -------------------------------------------------------------
 output_file <- "k:/dept/DIGITAL E-COMMERCE/E-COMMERCE/Report E-Commerce/pim_report/raw data/clx_reshape.csv"
 
-anagrafica %>% 
+out <- anagrafica %>% 
         select(ID_ARTICOLO,SKUs) %>% 
         mutate(sku = str_split(string = SKUs, pattern = ",")) %>% 
         unnest(sku) %>% 
         select(-SKUs) %>% 
         left_join(clx %>% select(-sku), by = c("ID_ARTICOLO" = "uid")) %>% 
-        distinct() %>% 
+        distinct() 
+
+
+out %>% 
         write.csv(na = "", row.names = F, file = output_file)
 
 
